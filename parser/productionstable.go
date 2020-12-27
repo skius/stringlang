e4383207e797c62736b0d3f9bfb121c0ad76eb7d
Niels Saurer <me@nielssaurer.com>
2020-12-27 17:05:05 +0100

    Update gocc & generated parser

--- parser/productionstable.go
+++ parser/productionstable.go
@@ -5,7 +5,6 @@ package parser
 import "github.com/skius/stringlang/ast"
 
 type (
-	//TODO: change type and variable names to be consistent with other tables
 	ProdTab      [numProductions]ProdTabEntry
 	ProdTabEntry struct {
 		String     string
