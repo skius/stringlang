b7b684daaec54472c91ef3a2fb8e4f43ceccff9a
Niels Saurer <me@nielssaurer.com>
2021-04-14 18:16:40 +0200

    Update REPL to use golang.org/x/term

--- cmd/stringlang/repl.go
+++ cmd/stringlang/repl.go
@@ -7,6 +7,7 @@ import (
 	"github.com/skius/stringlang/ast"
 	"github.com/skius/stringlang/errors"
 	"github.com/skius/stringlang/token"
+	"golang.org/x/term"
 	"os"
 	"runtime"
 	"strings"
@@ -25,40 +26,33 @@ var Gray   = "\033[37m"
 var White  = "\033[97m"
 
 func repl() {
-	if runtime.GOOS == "windows" {
-		Reset  = ""
-		Red    = ""
-		Green  = ""
-		Yellow = ""
-		Blue   = ""
-		Purple = ""
-		Cyan   = ""
-		Gray   = ""
-		White  = ""
-	}
 
-	fmt.Println("Welcome to the StringLang REPL!")
-	fmt.Println("Enter code, run it by pressing ENTER, repeat!")
-	fmt.Println("Note: This REPL stores all code you've entered and will execute everything each time you enter new code.")
-	fmt.Println("Reset your program using 'reset;;' and quit the REPL using 'quit;;'.")
+	// _ is missing setIndent
+	printLn, readLn, cleanup, _ := initTerminal()
+	defer cleanup()
+
+	printLn("Welcome to the StringLang REPL!")
+	printLn("Enter code, run it by pressing ENTER, repeat!")
+	printLn("Note: This REPL stores all code you've entered and will execute everything each time you enter new code.")
+	printLn("Reset your program using 'reset;;' and quit the REPL using 'quit;;'.")
 
 	program := ast.EmptyProgram()
-	stdin := bufio.NewReader(os.Stdin)
 
 	repl:
 	for {
+		indentLevel := 0
 		input := ""
 		var newExpr stringlang.Expr
+		// Read-parse loop until we either know input can't be valid a StringLang fragment, or we have a successful parse
 		for {
-			temp, err := stdin.ReadString('\n')
-			if err != nil {
-				panic(err)
-			}
+			// Uncomment for automated basic and bad indentation support
+			// setIndent(indentLevel)
+			temp := readLn()
 
 			// Special keywords
 			trimmedTemp := strings.TrimSpace(temp)
 			if strings.HasSuffix(trimmedTemp, "reset;;") {
-				fmt.Println("Resetting REPL... Reset!")
+				printLn("Resetting REPL... Reset!")
 				program = ast.EmptyProgram()
 				continue repl
 			}
@@ -66,28 +60,35 @@ func repl() {
 				break repl
 			}
 
+			// Check if we need to change indentation
+			opens := strings.Count(temp, "{")
+			closes := strings.Count(temp, "}")
+			indentLevel = indentLevel + opens - closes
+
 			input += temp
 
+			var err error
 			newExpr, err = stringlang.Parse([]byte(input))
 			if err == nil {
-				// newExpr got successfully parsed into a program, let's execute it.
+				// newExpr got successfully parsed into a program, let's execute it
 				break
 			}
 			pErr := err.(*errors.Error)
 			if pErr.ErrorToken.Type == token.EOF {
 				// If err is of the form "expected <something>; got: end-of-file", we know the program was potentially
-				// correct, just incomplete, so we should keep reading.
+				// correct, just incomplete, so we should keep reading
 				continue
 			}
 			unexpectedToken := pErr.ErrorToken.Lit
 			if strings.HasPrefix(string(unexpectedToken), `"`) {
-				// Start of a multiline string, keep reading.
+				// Start of a multiline string, keep reading
 				continue
 			}
 
 			// Otherwise there's no chance the program could become correct, so we have to reset
-			fmt.Println("There was an error parsing your input: ", err)
+			printLn("There was an error parsing your input: ", err)
 			input = ""
+			indentLevel = 0
 		}
 
 		// Combine the program extension with the old program
@@ -102,27 +103,91 @@ func repl() {
 
 		result, err := evalOrTimeout(program, time.Second * 30)
 		if err != nil {
-			fmt.Println("There was an error running your program: ", err)
+			printLn("There was an error running your program: ", err)
 			continue
 		}
-		fmt.Println(Yellow + result + Reset)
+		printLn(Yellow + result + Reset)
+
 	}
 
-	fmt.Println("Exiting REPL.")
+	printLn("Exiting REPL.")
 }
 
-func readLines() string {
-	in    := bufio.NewReader(os.Stdin)
-	input := ""
+func initTerminal() (func(...interface{}), func() string, func(), func(int)) {
+	var printLn func(...interface{})
+	var readLn func() string
+	var cleanup = func() {}
+	var setIndent func(int)
 
-	// Read in lines of StringLang until ;;
-	for !strings.HasSuffix(input, ";;") {
-		temp, err := in.ReadString('\n')
+
+	if runtime.GOOS == "windows" {
+		Reset  = ""
+		Red    = ""
+		Green  = ""
+		Yellow = ""
+		Blue   = ""
+		Purple = ""
+		Cyan   = ""
+		Gray   = ""
+		White  = ""
+
+		var indent int
+		setIndent = func(i int) {
+			indent = i
+		}
+
+		stdin := bufio.NewReader(os.Stdin)
+		printLn = func(a ...interface{}) {
+			fmt.Println(a...)
+		}
+		readLn = func() string {
+			fmt.Print(genSpaces(indent * 4))
+			s, err := stdin.ReadString('\n')
+			if err != nil {
+				panic(err)
+			}
+			// ReadString returns line incl. delimiter = line-break, so keep 's' as is
+			return s
+		}
+	} else {
+		// Can use proper terminal
+		oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 		if err != nil {
 			panic(err)
 		}
-		temp = strings.TrimSpace(temp)
-		input += " " + temp
+
+		t := term.NewTerminal(os.Stdin, "> ")
+
+		setIndent = func(i int) {
+			// Need to add "> " if we want it even with indentation
+			t.SetPrompt(genSpaces(i * 4))
+		}
+
+		printLn = func(a ...interface{}) {
+			_, err := t.Write([]byte(fmt.Sprintln(a...)))
+			if err != nil {
+				panic(err)
+			}
+		}
+		readLn = func() string {
+			s, err := t.ReadLine()
+			if err != nil {
+				panic(err)
+			}
+			// Terminal.ReadLine returns line without line-break, so let's add it to s
+			return s + "\n"
+		}
+		cleanup = func() {
+			term.Restore(int(os.Stdin.Fd()), oldState)
+		}
 	}
-	return strings.TrimSpace(input)
+	return printLn, readLn, cleanup, setIndent
+}
+
+func genSpaces(i int) string {
+	s := ""
+	for j := 0; j < i; j++ {
+		s += " "
+	}
+	return s
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/skius/stringlang
 
 go 1.15
+
+require (
+	golang.org/x/term v0.0.0-20210406210042-72f3dc4e9b72
+)
